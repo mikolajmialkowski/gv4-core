612e31899e8203efc6d63fd4e2fc82db403f8d70
mikolajmialkowski <mikolaj.mialkowski@outlook.com>
2022-08-04 11:02:38 +0200

    initial commit

---
+++ src/test/java/com/gv4/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gv4.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
