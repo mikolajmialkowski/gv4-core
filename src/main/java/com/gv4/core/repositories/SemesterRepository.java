1b826beb344929a46aedd91dad2a2b433ede418a
Jacke <24881@pjwstk.edu.pl>
2022-08-17 22:41:07 +0200

    updated SemesterService

--- src/main/java/com/gv4/core/repositories/SemesterRepository.java
+++ src/main/java/com/gv4/core/repositories/SemesterRepository.java
@@ -2,8 +2,13 @@ package com.gv4.core.repositories;
 
 import com.gv4.core.models.entities.SemesterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SemesterRepository extends JpaRepository<SemesterEntity, Long> {
+    @Query("select s from SemesterEntity s")
+    List<SemesterEntity> findBySemesterNumber();
 }
--- src/main/java/com/gv4/core/services/SemesterService.java
+++ src/main/java/com/gv4/core/services/SemesterService.java
@@ -1,10 +1,21 @@
 package com.gv4.core.services;
 
+import com.gv4.core.models.business.SemesterDTO;
+import com.gv4.core.models.entities.SemesterEntity;
 import com.gv4.core.repositories.SemesterRepository;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SemesterService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SemesterService.class);
 
     private final SemesterRepository semesterRepository;
 
@@ -12,6 +23,37 @@ public class SemesterService {
         this.semesterRepository = semesterRepository;
     }
 
-    //TODO (...)
+    public List<SemesterDTO> getSemesters(){
+        return semesterRepository.findAll().stream().map(this::getSemesterDTO).collect(Collectors.toList());
+    }
 
+    @SneakyThrows
+    public SemesterDTO getSemester(Long id){
+        return getSemesterDTO(semesterRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No semester for id = " + id)));
+    }
+
+    public SemesterDTO addSemester(SemesterDTO semesterDTO){
+        semesterRepository.findBySemesterNumber(semesterDTO.getAlias())
+                .ifPresent((duplicate) -> {
+                    throw new IllegalFieldException("Values have to be unique for alias = " + subjectDTO.getAlias());
+                });
+
+        return getSubjectDTO(subjectRepository.saveAndFlush(getSubjectEntity(subjectDTO)));
+    }
+
+    private SemesterDTO getSemesterDTO(SemesterEntity semesterEntity){
+        return SemesterDTO.builder()
+                .id(semesterEntity.getId())
+                .name(semesterEntity.getName())
+                .alias(semesterEntity.getAlias())
+                .build();
+    }
+
+    private SemesterEntity getSemesterEntity(SemesterDTO semesterDTO){
+        return SemesterEntity.builder()
+                .name(semesterDTO.getName())
+                .alias(semesterDTO.getAlias())
+                .build();
+    }
 }
